1f50c8d72cad9d272846823e0a9190d6ac9ab034
STEVE IAN M. <online.ianmakes@gmail.com>
2026-05-13 18:56:38 +0300

    Update test_db.php with error reporting

--- test_db.php
+++ test_db.php
@@ -1,8 +1,14 @@
 <?php
-$servername = "localhost";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "127.0.0.1";
 $username = "u826514770_viennapos3";
 $password = "mE4sU3h";
 $dbname = "u826514770_viennapos3";
+
+echo "Testing connection to $servername...<br>";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
